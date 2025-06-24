d0e5569c7be3ceeaf0bb0497ff2becc4731eb38b
Prathamesh Potekar <potekarprathamesh362@gmail.com>
2025-06-24 12:48:37 +0530

    Update Runner.java

--- orange_hrm_02/src/test/java/run/Runner.java
+++ orange_hrm_02/src/test/java/run/Runner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "C:\\Users\\AC\\eclipse-workspace\\orange_hrm_02\\feature\\reset_password.feature",
+@CucumberOptions(features= "Feature File Path",
 				 glue ="steodefination",
 				 plugin = {"pretty", "html:target/cucumber-reports.html"
 				 		}
